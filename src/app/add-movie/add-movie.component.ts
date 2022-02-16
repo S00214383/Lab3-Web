f0889826e02bdeaea8b46f5dffb7d0068db65beb
rudgerylopes94 <63984559+rudgerylopes94@users.noreply.github.com>
2022-02-16 17:50:24 +0000

    add movie, nested/child components, bootstrap

---
+++ src/app/add-movie/add-movie.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../movie.service';
+
+@Component({
+  selector: 'app-add-movie',
+  templateUrl: './add-movie.component.html',
+  styleUrls: ['./add-movie.component.css']
+})
+
+
+
+export class AddMovieComponent implements OnInit {
+
+  constructor(private movieService: MovieService) { }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+
+  addNewMovie(movietitle:HTMLInputElement, moviedirector:HTMLInputElement, movieyear:HTMLInputElement):boolean{
+    this.movieService.addMovies(movietitle.value, moviedirector.value, movieyear.value);
+    return false;
+  }
+  
+
+
+ 
+  
+  
+
+}
